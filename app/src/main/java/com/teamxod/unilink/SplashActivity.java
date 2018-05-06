64a5b98bb71d77b1ff95bd6e9d5c38d842c46b83
SudoToEat <13689064+SudoToEat@users.noreply.github.com>
2018-05-06 02:10:12 -0700

    finish SplashActivity

--- app/src/main/java/com/teamxod/unilink/SplashActivity.java
+++ app/src/main/java/com/teamxod/unilink/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.teamxod.unilink;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 // Firebase
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +23,29 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         mAuth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+
+                // need register or login
+                if (currentUser == null) {
+                    Intent startIntent = new Intent(SplashActivity.this,StartActivity.class);
+                    startActivity(startIntent);
+                    finish();
+
+                // already logged in, go to home page
+                } else {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+
+            }
+
+            },SPLASH_TIME
+        );
 
     };
 
