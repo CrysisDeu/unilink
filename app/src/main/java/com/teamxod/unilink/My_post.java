e55c1a992a9f03ee3d2ee8217ae0418da5158f76
root <root@DESKTOP-0VVC50T.localdomain>
2018-05-05 22:39:34 -0700

    MyPage: java files ; XML files

---
+++ app/src/main/java/com/teamxod/unilink/My_post.java
@@ -0,0 +1,13 @@
+package com.example.user.mypage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class My_post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_post);
+    }
+}
