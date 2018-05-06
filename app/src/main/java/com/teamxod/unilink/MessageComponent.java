6d8677d07c65b8284b6c997a24b3ce44727e4c80
CrysisDeu <1420973012@qq.com>
2018-05-06 16:37:24 -0700

    added message component class

---
+++ app/src/main/java/com/teamxod/unilink/MessageComponent.java
@@ -0,0 +1,52 @@
+package com.teamxod.unilink;
+
+public class MessageComponent {
+    private String userPhoto;
+    private String name;
+    private String text;
+    private String messagephoto;
+
+    public MessageComponent() {
+    }
+
+    public MessageComponent(String userPhoto, String text, String name, String messagephoto) {
+        this.userPhoto = userPhoto;
+        this.text = text;
+        this.name = name;
+        this.messagephoto = messagephoto;
+    }
+
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String photoUrl) {
+        this.userPhoto = photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getmessagePhoto() {
+        return messagephoto;
+    }
+
+    public void setMessagephoto(String photoUrl) {
+        this.messagephoto = photoUrl;
+    }
+}
+
