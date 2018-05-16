2055fb08a45a50703ba793e7e87a67050c18a908
unknown <weh073@ucsd.edu>
2018-05-15 18:14:52 -0700

    connect single house post to house post list, house post still in progress

--- app/src/main/java/com/teamxod/unilink/SingleHousePostActivity.java
+++ app/src/main/java/com/teamxod/unilink/SingleHousePostActivity.java
@@ -2,6 +2,7 @@ package com.teamxod.unilink;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,21 @@ import android.view.ViewGroup;
 
 public class SingleHousePostActivity extends AppCompatActivity {
 
+    ViewPager housePicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_house_post);
+        setContentView(R.layout.house_post);
+
+        //House house = new House();
+
+        housePicture = (ViewPager)findViewById(R.id.house_image);
+
+        //HousePictureAdapter housePictureAdapter = new HousePictureAdapter(this, house);
+        HousePictureAdapter housePictureAdapter = new HousePictureAdapter(this);
+
+        housePicture.setAdapter(housePictureAdapter);
     }
 
 }
