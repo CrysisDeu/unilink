ce2c27a9a45675a6d8c827436627737ddf94b7b0
CrysisDeu <1420973012@qq.com>
2018-05-15 17:00:43 -0700

    test1

--- app/src/main/java/com/teamxod/unilink/SplashActivity.java
+++ app/src/main/java/com/teamxod/unilink/SplashActivity.java
@@ -1,52 +1,15 @@
 package com.teamxod.unilink;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
-// Firebase
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private static int SPLASH_TIME = 2000;
-
-    // Firebase
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        mAuth = FirebaseAuth.getInstance();
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                FirebaseUser currentUser = mAuth.getCurrentUser();
-
-                // need register or login
-                if (currentUser == null) {
-                    Intent startIntent = new Intent(SplashActivity.this, StartActivity.class);
-                    startActivity(startIntent);
-                    finish();
-
-                // already logged in, go to home page
-                } else {
-                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
-                }
-
-            }
-
-            },SPLASH_TIME
-        );
-
-    };
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
