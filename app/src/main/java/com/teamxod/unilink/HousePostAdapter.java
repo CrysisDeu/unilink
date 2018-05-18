c5bb72553043e2b6dd4d149cd3c5a49a11b9ecee
dongweicong <w9dong@ucsd.edu>
2018-05-17 18:12:21 -0700

    change

--- app/src/main/java/com/teamxod/unilink/HousePostAdapter.java
+++ app/src/main/java/com/teamxod/unilink/HousePostAdapter.java
@@ -1,6 +1,5 @@
 package com.teamxod.unilink;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
