6078c8a9e9a0e315781e21cf2f32f04b7d0f81bd
neal3560 <weh073@ucsd.edu>
2018-05-17 21:28:05 -0700

    google map

--- app/src/main/java/com/teamxod/unilink/SingleHousePostActivity.java
+++ app/src/main/java/com/teamxod/unilink/SingleHousePostActivity.java
@@ -6,38 +6,80 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 
-public class SingleHousePostActivity extends AppCompatActivity {
+public class SingleHousePostActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private ViewPager housePicture;
-    private RecyclerView roommateListView;
+    private MapFragment mapFragment;
+
+    private House house;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.house_post);
 
+        //data needed later from database
 
-        //House house = new House();
-        //HousePictureAdapter housePictureAdapter = new HousePictureAdapter(this, house);
-        housePicture = (ViewPager)findViewById(R.id.house_image);
-        HousePictureAdapter housePictureAdapter = new HousePictureAdapter(this);
+        house = new House();
+        ArrayList<User> roommateList;
+        roommateList = new ArrayList<User>();
+        roommateList.add(new User());
+        roommateList.add(new User());
+
+        //google map
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.house_map);
+        mapFragment.getMapAsync(this);
+
+        //house picture list setting
+        ViewPager housePicture = (ViewPager)findViewById(R.id.house_image);
+        HousePictureAdapter housePictureAdapter = new HousePictureAdapter(this);//housePictureAdapter = new HousePictureAdapter(this, house);
         housePicture.setAdapter(housePictureAdapter);
 
-
-        //test statements
-        ArrayList<User> roommateList = new ArrayList<User>();
-        roommateList.add(new User());
-        roommateList.add(new User());
-
-        roommateListView = (RecyclerView) findViewById(R.id.house_roommate);
+        //roommate list setting
+        RecyclerView roommateListView = (RecyclerView) findViewById(R.id.house_roommate);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         roommateListView.setLayoutManager(layoutManager);
         roommateListView.setHasFixedSize(true);
-
         UserPictureAdapter roommateAdapter = new UserPictureAdapter(roommateList);
         roommateListView.setAdapter(roommateAdapter);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapFragment.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapFragment.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapFragment.onLowMemory();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapFragment.onPause();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title(house.getName()));
+    }
 }
