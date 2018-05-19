ed70794489d8da0eee097efe44a2be8f716e4eda
YLJALDC <dal177@ucsd.edu>
2018-05-19 00:09:48 -0700

    two-checkText complete

--- app/src/main/java/com/teamxod/unilink/My_preference.java
+++ app/src/main/java/com/teamxod/unilink/My_preference.java
@@ -1,11 +1,10 @@
 package com.teamxod.unilink;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,33 +16,24 @@ public class My_preference extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_my_preference);
+        setContentView(R.layout.activity_my_preference);
         // create the list of questions
         questionList = new ArrayList<Question>();
         // add questions to the arrayList
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-        questionList.add(new Question("Nihao", false));
-
+        questionList.add(new Question("Nihao", "diyi", "dier"));
 
         // set adaptor to display
         preferenceAdaptor adaptor = new preferenceAdaptor(this, questionList);
         listView = (ListView) findViewById(R.id.my_preference_list);
         listView.setAdapter(adaptor);
+
+        /**change state while selecting
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CheckedTextView
+            }
+        });*/
 
     }
 }
--- app/src/main/java/com/teamxod/unilink/preferenceAdaptor.java
+++ app/src/main/java/com/teamxod/unilink/preferenceAdaptor.java
@@ -8,38 +8,55 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageView;
+import android.widget.CheckedTextView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
 class Question{
-    // q is the acual question and checked is the indicator to show if this question is checked or not
+    // q is the question and choiceOne/Two are choices
+    // booleans are indicators for boxes checked or not. Stored to implement check only one functionality
     String q;
-    boolean checked;
+    String choiceOne;
+    String choiceTwo;
+    boolean firstChecked;
+    boolean secondChecked;
 
-    public Question(String q, boolean checked) {
+    public Question(String q, String choiceOne, String choiceTwo) {
         this.q = q;
-        this.checked = checked;
+        this.firstChecked = false;
+        this.secondChecked = false;
+        this.choiceOne = choiceOne;
+        this.choiceTwo = choiceTwo;
     }
-
+    // getter and setter for fields
     public String getQuestion(){
         return this.q;
     }
 
-    public boolean getChecked(){
-        return this.checked;
+    public String getChoiceOne() { return this.choiceOne;}
+
+    public String getChoiceTwo() { return this.choiceTwo;}
+
+    public boolean getfirstChecked(){
+        return this.firstChecked;
     }
+
+    public boolean getSecondChecked() {return this.secondChecked;}
 
     public void setQuestion(String q){
         this.q = q;
     }
 
-    public void setChecked(boolean checked){
-        this.checked = checked;
+    public void setChoiceOne(String s) { this.choiceOne = s;}
+
+    public void setChoiceTwo(String s) { this.choiceTwo = s;}
+
+    public void setfirstChecked(boolean checked){
+        this.firstChecked = checked;
     }
+
+    public void setSecondChecked(boolean checked) {this.secondChecked = checked;}
 
 
 }
@@ -57,12 +74,43 @@ public class preferenceAdaptor extends ArrayAdapter<Question>{
         View listItem = view;
         if(listItem == null) {
             listItem = LayoutInflater.from(getContext()).inflate(
-                    R.layout.preference_list_item_check, parent, false);
+                    R.layout.preference_list_item_check_two, parent, false);
         }
-        Question preference = (Question)getItem(position);
+        // set Question's text fields
+        final Question preference = (Question)getItem(position);
+
         TextView q = (TextView)listItem.findViewById((R.id.question));
         q.setText(preference.getQuestion());
-        CheckBox checkBox = (CheckBox)listItem.findViewById(R.id.checkBox);
+
+        CheckedTextView checkOne = (CheckedTextView)listItem.findViewById(R.id.checked1);
+        checkOne.setText(preference.getChoiceOne());
+
+        CheckedTextView checkTwo = (CheckedTextView)listItem.findViewById((R.id.checked2));
+        checkTwo.setText(preference.getChoiceTwo());
+
+        checkOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!preference.getfirstChecked()){
+                    preference.setfirstChecked(true);
+                    preference.setSecondChecked(false);
+                }else{
+                    preference.setfirstChecked(false);
+                }
+            }
+        });
+
+        checkTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!preference.getSecondChecked()) {
+                    preference.setfirstChecked(false);
+                    preference.setSecondChecked(true);
+                }else{
+                    preference.setSecondChecked(false);
+                }
+            }
+        });
 
         return listItem;
     }
