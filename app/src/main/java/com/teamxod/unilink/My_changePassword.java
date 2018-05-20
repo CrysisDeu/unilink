10dc7bdc7cc12535e7b347033564ddb62bb1314c
root <root@DESKTOP-0VVC50T.localdomain>
2018-05-19 21:07:47 -0700

    update forgot_password and change_profile

--- app/src/main/java/com/teamxod/unilink/My_changePassword.java
+++ app/src/main/java/com/teamxod/unilink/My_changePassword.java
@@ -22,6 +22,7 @@ public class My_changePassword extends AppCompatActivity {
     private EditText new_password;
     private EditText confirm_password;
     private TextView done;
+    private TextView forget_password;
 
     private final int done_orig_size = 13;
 
@@ -40,6 +41,7 @@ public class My_changePassword extends AppCompatActivity {
         new_password = (EditText) findViewById(R.id.new_password);
         confirm_password = (EditText) findViewById(R.id.confirm_password);
         done = (TextView) findViewById(R.id.Done);
+        forget_password = (TextView) findViewById(R.id.forget_password);
 
         original_password.addTextChangedListener(new TextWatcher() {
 
@@ -147,6 +149,20 @@ public class My_changePassword extends AppCompatActivity {
             }
         });
 
+        forget_password.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // hide kb
+                View v = findViewById(android.R.id.content);
+                if (v != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+                Snackbar.make(findViewById(R.id.Coordinator), "Your password is sent to your account email !",
+                        Snackbar.LENGTH_LONG).show();
+
+            }
+        });
 
     }
 }
