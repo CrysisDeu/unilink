0e306e282ac95674820eccebf740acf76bcb35b9
dongweicong <w9dong@ucsd.edu>
2018-05-20 19:22:04 -0700

    update add post btn

--- app/src/main/java/com/teamxod/unilink/HousingFragment.java
+++ app/src/main/java/com/teamxod/unilink/HousingFragment.java
@@ -27,6 +27,7 @@ public class HousingFragment extends Fragment {
     ListView listView;
     LinearLayout searchBar;
     Button filterButton;
+    Button addPost;
     View header;
     int touchSlop = 5;
 
@@ -49,6 +50,7 @@ public class HousingFragment extends Fragment {
         listView = (ListView) layout.findViewById(R.id.list_view);
         searchBar = layout.findViewById(R.id.searchView);
         filterButton = layout.findViewById(R.id.filter);
+        addPost = layout.findViewById(R.id.add_post_btn);
 
         HousePostAdapter adapter = new HousePostAdapter(this.getActivity(), posts,listView);
         listView.setAdapter(adapter);
@@ -96,15 +98,16 @@ public class HousingFragment extends Fragment {
 
         } else {
             backAnimatorSet = new AnimatorSet();
+
             //move the element back to originial position
             ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(searchBar, "translationY", searchBar.getTranslationY(), 0f);
-            //ObjectAnimator listAnimator = ObjectAnimator.ofFloat(listView, "translationY", listView.getTranslationY(), 0f);
+            ObjectAnimator addPostAnimator = ObjectAnimator.ofFloat(addPost, "translationY", addPost.getTranslationY(), 0f);
 
             //add animator object to arraylist
             ArrayList<Animator> animators = new ArrayList<>();
             animators.add(headerAnimator);
-            //animators.add(listAnimator);
-            //animators.add(naviAnimator);
+            animators.add(addPostAnimator);
+
 
             backAnimatorSet.setDuration(400);
             backAnimatorSet.playTogether(animators);
@@ -126,13 +129,13 @@ public class HousingFragment extends Fragment {
         } else {
             hideAnimatorSet = new AnimatorSet();
             ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(searchBar, "translationY", searchBar.getTranslationY(), -2* searchBar.getHeight());
-            //ObjectAnimator listAnimator = ObjectAnimator.ofFloat(listView, "translationY", listView.getTranslationY(), -0.11f*listView.getHeight());
+            ObjectAnimator addPostAnimator = ObjectAnimator.ofFloat(addPost, "translationY", addPost.getTranslationY(), 2*addPost.getHeight());
 
             ArrayList<Animator> animators = new ArrayList<>();
             animators.add(headerAnimator);
-            //animators.add(listAnimator);
+            animators.add(addPostAnimator);
 
-            hideAnimatorSet.setDuration(300);
+            hideAnimatorSet.setDuration(400);
             hideAnimatorSet.playTogether(animators);
             hideAnimatorSet.start();
         }
