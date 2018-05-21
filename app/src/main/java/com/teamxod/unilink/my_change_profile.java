29d16824184556b0e5f6976dc4f7d627c67a7e80
CrysisDeu <1420973012@qq.com>
2018-05-21 01:26:40 -0700

    修改change password界面

--- app/src/main/java/com/teamxod/unilink/my_change_profile.java
+++ app/src/main/java/com/teamxod/unilink/my_change_profile.java
@@ -15,7 +15,7 @@ public class my_change_profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_change_profile);
 
-        CardView saveProfile = findViewById(R.id.save_profile);
+        CardView saveProfile = findViewById(R.id.save);
         /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             StateListAnimator stateListAnimator = AnimatorInflater
                     .loadStateListAnimator(this, R.animator.lift_on_touch);
