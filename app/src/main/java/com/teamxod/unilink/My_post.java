06f31d6b260aa998d35742ad971142afbf21e5e2
CrysisDeu <32421101+crysisdeu@users.noreply.github.com>
2018-05-22 11:37:35 -0700

    加backbutton

--- app/src/main/java/com/teamxod/unilink/My_post.java
+++ app/src/main/java/com/teamxod/unilink/My_post.java
@@ -1,12 +1,24 @@
 package com.teamxod.unilink;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class My_post extends AppCompatActivity {
+
+    private ImageView mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_post);
+
+        mBackButton = findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/teamxod/unilink/my_change_profile.java
+++ app/src/main/java/com/teamxod/unilink/my_change_profile.java
@@ -6,20 +6,38 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class my_change_profile extends AppCompatActivity {
 
+    private ImageView mBackButton;
+
     private CardView saveProfile;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_change_profile);
 
-        CardView saveProfile = findViewById(R.id.save);
+        mBackButton = findViewById(R.id.back_button);
+
+        saveProfile = findViewById(R.id.save);
         /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             StateListAnimator stateListAnimator = AnimatorInflater
                     .loadStateListAnimator(this, R.animator.lift_on_touch);
             card.setStateListAnimator(stateListAnimator);
         }*/
+
+
+        mBackButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
