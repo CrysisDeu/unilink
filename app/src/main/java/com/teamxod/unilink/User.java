a9bfe8eb1339fba78b9c239e050f71cdb8f12d7f
SudoToEat <13689064+sudotoeat@users.noreply.github.com>
2018-05-26 17:14:13 -0700

    update change user profile

--- app/src/main/java/com/teamxod/unilink/User.java
+++ app/src/main/java/com/teamxod/unilink/User.java
@@ -1,8 +1,12 @@
 package com.teamxod.unilink;
 
+import com.google.firebase.database.Exclude;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -10,8 +14,8 @@ public class User {
     private String gender;
     private String yearGraduate;
     private String description;
-    private List<String> favorite_houses;
-    private List<String> my_house_posts;
+    private List<String> favorite_houses = new ArrayList<>(0);
+    private List<String> my_house_posts = new ArrayList<>(0);
 
     //temporary constructor
     User() {
@@ -81,6 +85,19 @@ public class User {
 
     public void setMy_house_posts(List<String> my_house_posts) {
         this.my_house_posts = my_house_posts;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("picture", picture);
+        result.put("gender", gender);
+        result.put("yearGraduate", yearGraduate);
+        result.put("description", description);
+        result.put("favorite_houses", favorite_houses);
+        result.put("my_house_posts",my_house_posts);
+        return result;
     }
 
     //FIXME
--- app/src/main/java/com/teamxod/unilink/my_change_profile.java
+++ app/src/main/java/com/teamxod/unilink/my_change_profile.java
@@ -4,6 +4,7 @@ import android.animation.AnimatorInflater;
 import android.animation.StateListAnimator;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -12,19 +13,29 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
 
 import java.util.Map;
 
-public class my_change_profile extends AppCompatActivity {
+public class my_change_profile extends AppCompatActivity implements IPickResult {
 
     //spinner selector
     private final int GENDER_MALE = 0;
@@ -48,9 +59,11 @@ public class my_change_profile extends AppCompatActivity {
 
     //Firebase
     private DatabaseReference mDatabase;
+    private StorageReference mStorageRef;
     private FirebaseAuth mAuth;
     private String uid;
     private User user;
+    private Uri picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +86,7 @@ public class my_change_profile extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         uid = mAuth.getCurrentUser().getUid();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         final DatabaseReference mUserReference = mDatabase.child("Users").child(uid);
 
         mUserReference.addValueEventListener(new ValueEventListener() {
@@ -81,7 +95,7 @@ public class my_change_profile extends AppCompatActivity {
                 user = dataSnapshot.getValue(User.class);
                 mEditName.setText(user.getName());
                 Uri mPhoto = (Uri.parse(user.getPicture()));
-                Glide.with(my_change_profile.this)
+                Glide.with(getApplicationContext())
                         .load(mPhoto)
                         .apply(RequestOptions.circleCropTransform())
                         .into(mProfilePic);
@@ -100,12 +114,25 @@ public class my_change_profile extends AppCompatActivity {
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                user.setName(mEditName.getText().toString());
-                user.setGender(mGenderSpinner.getSelectedItem().toString());
-                user.setYearGraduate(mYearSpinner.getSelectedItem().toString());
-                user.setDescription(mDescription.getText().toString());
-                mUserReference.setValue(user);
+//                user.setName(mEditName.getText().toString());
+//                user.setGender(mGenderSpinner.getSelectedItem().toString());
+//                user.setYearGraduate(mYearSpinner.getSelectedItem().toString());
+//                user.setDescription(mDescription.getText().toString());
+//                mUserReference.setValue(user);
+                uploadToFirebase(picture);
+                mUserReference.child("name").setValue(mEditName.getText().toString());
+                mUserReference.child("gender").setValue(mGenderSpinner.getSelectedItem().toString());
+                mUserReference.child("yearGraduate").setValue(mYearSpinner.getSelectedItem().toString());
+                mUserReference.child("description").setValue(mDescription.getText().toString());
+                mUserReference.child("picture").setValue(picture.toString());
                 finish();
+            }
+        });
+        mProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PickImageDialog.build(new PickSetup()).show(my_change_profile.this);
+
             }
         });
 
@@ -117,6 +144,58 @@ public class my_change_profile extends AppCompatActivity {
         });
         //}
     }
+
+    //PickImage Plug-in
+    //choose picture from camera or gallery
+    @Override
+    public void onPickResult(PickResult r) {
+        if (r.getError() == null) {
+
+            //Mandatory to refresh image from Uri.
+            // upload to firebase storage
+
+            picture = r.getUri();
+            Glide.with(getApplicationContext())
+                    .load(picture)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(mProfilePic);
+        } else {
+            Toast.makeText(this, r.getError().getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void uploadToFirebase (Uri uri) {
+        final StorageReference profile_images = mStorageRef.child("Profile_Images").child(mAuth.getCurrentUser().getUid());
+
+        profile_images.putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        profile_images.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                picture = uri;
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle any errors
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+
+
+    }
+
 
     private int getGenderSelection(String gender) {
         int genderSelection;
