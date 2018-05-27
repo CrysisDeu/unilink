4d4c079a1bd10223909a5134dfaf0b0902a312fe
root <root@DESKTOP-0VVC50T.localdomain>
2018-05-26 22:46:51 -0700

    AddPost.java(pass user input to firebase) 70% done; XML 60%(need grid for img, snackbar updated)

--- app/src/main/java/com/teamxod/unilink/AddPost.java
+++ app/src/main/java/com/teamxod/unilink/AddPost.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -344,6 +345,9 @@ public class AddPost extends AppCompatActivity {//TODO implements IPickResult {
                 _description = description.getText().toString();
 
                 writeNewPost();
+
+                // notify user submitted
+                Snackbar.make(findViewById(R.id.Coordinator), "Congrudulation, you successfully post your house! " , Snackbar.LENGTH_LONG).show();
             }
         });
 
