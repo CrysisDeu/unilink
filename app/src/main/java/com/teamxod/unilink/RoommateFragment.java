61f846057c64dc3f8ca914ae5913ac8921f01b79
dongweicong <w9dong@ucsd.edu>
2018-05-31 15:04:52 -0700

    add animator to roomate layout

--- app/src/main/java/com/teamxod/unilink/RoommateFragment.java
+++ app/src/main/java/com/teamxod/unilink/RoommateFragment.java
@@ -1,26 +1,36 @@
 package com.teamxod.unilink;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
 public class RoommateFragment extends Fragment {
 
-    private String mNames[] = {"welcome","android","TextView"};
+    View header;
+    int touchSlop = 5;
     private ListView listView;
 
     private Button preference;
     private ToggleButton toggle;
 
+    private RelativeLayout layer;
     private TagViewGroup tagViewLayout;
 
     @Override
@@ -30,7 +40,7 @@ public class RoommateFragment extends Fragment {
         listView = layout.findViewById(R.id.roomate_list);
         preference = layout.findViewById(R.id.roommate_preference);
         toggle = layout.findViewById(R.id.toggle_score);
-
+        layer = layout.findViewById(R.id.layer);
 
         ArrayList<RoommateSimple> roommates = new ArrayList<>();
 
@@ -64,9 +74,117 @@ public class RoommateFragment extends Fragment {
                 }
             }
         });
+
+        header = new View(this.getActivity());
+        header.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) getResources().getDimension(R.dimen.header_roommate)));
+        header.setBackgroundColor(Color.parseColor("#00000000"));
+        listView.addHeaderView(header);
+        touchSlop = (int) (ViewConfiguration.get(RoommateFragment.this.getActivity()).getScaledTouchSlop() * 0.9);
         return layout;
     }
 
+
+
+    //set the back animator
+    AnimatorSet backAnimatorSet;
+
+    private void animateBack() {
+        //eliminate other animator
+        if (hideAnimatorSet != null && hideAnimatorSet.isRunning()) {
+            hideAnimatorSet.cancel();
+        }
+        if (backAnimatorSet != null && backAnimatorSet.isRunning()) {
+
+        } else {
+            backAnimatorSet = new AnimatorSet();
+
+            //move the element back to originial position
+            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(layer, "translationY", layer.getTranslationY(), 0f);
+
+            //add animator object to arraylist
+            ArrayList<Animator> animators = new ArrayList<>();
+            animators.add(headerAnimator);
+
+
+            backAnimatorSet.setDuration(400);
+            backAnimatorSet.playTogether(animators);
+            backAnimatorSet.start();
+        }
+    }
+
+
+    //aniamtor to hide element
+    AnimatorSet hideAnimatorSet;
+
+    private void animateHide() {
+        //eliminate other animators
+        if (backAnimatorSet != null && backAnimatorSet.isRunning()) {
+            backAnimatorSet.cancel();
+        }
+        if (hideAnimatorSet != null && hideAnimatorSet.isRunning()) {
+
+        } else {
+            hideAnimatorSet = new AnimatorSet();
+            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(layer, "translationY", layer.getTranslationY(), -2*layer.getHeight());
+
+            ArrayList<Animator> animators = new ArrayList<>();
+            animators.add(headerAnimator);
+
+            hideAnimatorSet.setDuration(400);
+            hideAnimatorSet.playTogether(animators);
+            hideAnimatorSet.start();
+        }
+    }
+
+
+
+    //set up onTouchListener
+    View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+
+
+        float lastY = 0f;
+        float currentY = 0f;
+        //represent two scroll direction  >0 : scroll down; <0 : scroll up
+        int lastDirection = 0;
+        int currentDirection = 0;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    lastY = event.getY();
+                    currentY = event.getY();
+                    currentDirection = 0;
+                    lastDirection = 0;
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    if (listView.getFirstVisiblePosition() > 0) {
+                        //hide or show element only if listView.getFirstVisiblePosition()>0
+                        float tmpCurrentY = event.getY();
+                        //start only if movement didtance > toushSlop
+                        if (Math.abs(tmpCurrentY - lastY) > touchSlop) {
+                            currentY = tmpCurrentY;
+                            currentDirection = (int) (currentY - lastY);
+                            if (lastDirection != currentDirection) {
+                                //if the direction of movement is different from lat time, then hide or show elements
+                                if (currentDirection < 0) {
+                                    animateHide();
+                                } else {
+                                    animateBack();
+                                }
+                            }
+                            lastY = currentY;
+                        }
+                    }
+                    break;
+
+                case MotionEvent.ACTION_CANCEL:
+
+            }
+            return false;
+        }
+    };
 
 
 
