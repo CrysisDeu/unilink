fc28f41f905eb2ba1028a829e1ccf8f43ca995b6
CrysisDeu <1420973012@qq.com>
2018-06-01 14:57:47 -0700

    改名字

--- app/src/main/java/com/teamxod/unilink/MyFavoriteActivity.java
+++ app/src/main/java/com/teamxod/unilink/MyFavoriteActivity.java
@@ -1,16 +1,11 @@
 package com.teamxod.unilink;
 
-import android.media.Image;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,7 +33,7 @@ public class MyFavoriteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_my_favorite);
+        setContentView(R.layout.activity_my_favorite);
 
         mBackButton = findViewById(R.id.back_button);
 
