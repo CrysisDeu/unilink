52a2d29f5f343000d6bd848a18022f0f6e5e9797
CrysisDeu <1420973012@qq.com>
2018-06-01 14:55:35 -0700

    Favorite 改回activity

--- app/src/main/java/com/teamxod/unilink/MyFavoriteActivity.java
+++ app/src/main/java/com/teamxod/unilink/MyFavoriteActivity.java
@@ -3,12 +3,14 @@ package com.teamxod.unilink;
 import android.media.Image;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,7 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class MyFavoriteFragment extends Fragment {
+public class MyFavoriteActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
@@ -30,35 +32,25 @@ public class MyFavoriteFragment extends Fragment {
 
     private RecyclerView houseListView;
 
+    private ImageView mBackButton;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_my_favorite, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_my_favorite);
 
-        setupButton(layout);
+        mBackButton = findViewById(R.id.back_button);
 
-        loadData(layout);
-
-        return layout;
-    }
-
-    private void setupButton(View layout) {
-        ImageView mBackButton = (ImageView) layout.findViewById(R.id.back_button);
-        mBackButton.setOnClickListener(new View.OnClickListener(){
+        mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment fragment = getFragmentManager().findFragmentByTag("favorite");
-                getFragmentManager()
-                        .beginTransaction()
-                        .remove(fragment)
-                        .commit();
+                finish();
             }
         });
-    }
 
-    private void loadData(View layout) {
-
-        houseListView = (RecyclerView) layout.findViewById(R.id.my_favorite_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        houseListView = findViewById(R.id.my_favorite_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MyFavoriteActivity.this, LinearLayoutManager.VERTICAL, false);
         houseListView.setLayoutManager(layoutManager);
         houseListView.setHasFixedSize(true);
         houseListView.addItemDecoration(new DividerItemDecoration(houseListView.getContext(), DividerItemDecoration.VERTICAL));
@@ -74,13 +66,15 @@ public class MyFavoriteFragment extends Fragment {
                 for (DataSnapshot postID : dataSnapshot.getChildren()) {
                     postList.add(postID.getValue(String.class));
                 }
-                favoriteListAdapter = new FavoriteAdapter(getActivity(), postList);
+                favoriteListAdapter = new FavoriteAdapter(MyFavoriteActivity.this, postList);
                 houseListView.setAdapter(favoriteListAdapter);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode()); }
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
         });
     }
+
 }
