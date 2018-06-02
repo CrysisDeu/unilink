ff95995f9a886f108ef4e4ce931365994de3e568
dongweicong <w9dong@ucsd.edu>
2018-06-02 14:44:42 -0700

    calculate match score

--- app/src/main/java/com/teamxod/unilink/RoommatePostActivity.java
+++ app/src/main/java/com/teamxod/unilink/RoommatePostActivity.java
@@ -4,11 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class RoommatePostActivity extends AppCompatActivity {
 
-    String uid;
+    private String currentUserUid;
+    private User currentUser;
+    private User roommate;
+    private String uid;
+    private Recommendation recommend;
+    private double matchScore;
+    private ArrayList<String> tags;
+    private DatabaseReference database;
+    private DatabaseReference userReference;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,11 +42,62 @@ public class RoommatePostActivity extends AppCompatActivity {
             Toast.makeText(this, "User does not exist", Toast.LENGTH_LONG).show();
             finish();
         }
+        //get current user and user in post
+        //loadData();
 
-        loadData();
+        recommend = new Recommendation(uid,uid);
+        matchScore = recommend.calculate();
+        tags = recommend.getTagList();
+        Log.d("check score","****"+matchScore);
+
+        //load tag
+
     }
 
     private void loadData() {
+        Intent intent = getIntent();
+
+        if(intent.hasExtra("postID")){
+            uid = intent.getExtras().getString("postID");
+        }else{
+            Toast.makeText(this, "People does not exist", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        //current user
+        currentUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        userReference = database.child("Users").child(currentUserUid);
+        userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                currentUser = dataSnapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        //roommate user
+        userReference = database.child("Users").child(uid);
+        userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                roommate = dataSnapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
 
     }
+
+
 }
