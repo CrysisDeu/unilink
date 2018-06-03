0528b2e5c3372b90a411eef4e697b7cc21cde64e
SudoToEat <13689064+SudoToEat@users.noreply.github.com>
2018-06-02 23:33:36 -0700

    add chatsfragment

---
+++ app/src/main/java/com/teamxod/unilink/chat/ChatsFragment.java
@@ -0,0 +1,216 @@
+package com.teamxod.unilink.chat;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.teamxod.unilink.Profile;
+import com.teamxod.unilink.R;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatsFragment extends Fragment {
+
+
+
+    // Firebase
+    private DatabaseReference mConvDatabase;
+    private DatabaseReference mMessageDatabase;
+    private DatabaseReference mChatDatabse;
+    private DatabaseReference mUsersDatabase;
+    private FirebaseRecyclerAdapter adapter;
+    private FirebaseAuth mAuth;
+    private String mCurrent_user_id;
+
+    // Views
+    private View mMainView;
+    private RecyclerView mConvList;
+
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Views
+        mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
+        mConvList = mMainView.findViewById(R.id.conv_list);
+
+        // Database
+        mAuth = FirebaseAuth.getInstance();
+        mCurrent_user_id = mAuth.getCurrentUser().getUid();
+        mConvDatabase = FirebaseDatabase.getInstance().getReference().child("Chat").child(mCurrent_user_id);
+        mConvDatabase.keepSynced(true);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mMessageDatabase = FirebaseDatabase.getInstance().getReference().child("Messages").child(mCurrent_user_id);
+        mChatDatabse = FirebaseDatabase.getInstance().getReference().child("Chat");
+        mUsersDatabase.keepSynced(true);
+
+        // Layout
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        mConvList.setHasFixedSize(true);
+        mConvList.setLayoutManager(linearLayoutManager);
+
+        return mMainView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Sort list by last chat time
+        Query conversationQuery = mConvDatabase.orderByChild("mTimestamp");
+        FirebaseRecyclerOptions<Chat> options =
+                new FirebaseRecyclerOptions.Builder<Chat>()
+                        .setQuery(conversationQuery, Chat.class)
+                        .build();
+
+        // adapter to hold contacts
+        adapter  = new FirebaseRecyclerAdapter<Chat, ChatListViewHolder>(options) {
+            @NonNull
+            @Override
+            public ChatListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.users_single_layout, parent, false);
+                return new ChatListViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull final ChatListViewHolder holder, int position, @NonNull Chat model) {
+
+                // bind last message to each holder
+                final String list_user_id = getRef(position).getKey();
+                Query lastMessageQuery = mMessageDatabase.child(list_user_id).limitToLast(1);
+                lastMessageQuery.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                        String data = dataSnapshot.child("mMessage").getValue().toString();
+                        Log.d("onBindViewHolder", data);
+                        holder.setMessage(data);
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                // set chat date
+                mChatDatabse.child(list_user_id).child(mCurrent_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild("mTimestamp")) {
+                            long time = Long.parseLong(dataSnapshot.child("mTimestamp").getValue().toString());
+                            Timestamp ts = new Timestamp(time * 1000);
+                            String tsStr = "";
+                            @SuppressLint("SimpleDateFormat") DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+                            tsStr = sdf.format(ts);
+                            holder.setChatTimeView(tsStr);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                // set contact image and name
+                mUsersDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        final String userName = dataSnapshot.child("name").getValue().toString();
+                        String userThumb = dataSnapshot.child("picture").getValue().toString();
+
+                        holder.setName(userName);
+                        holder.setUserImage(userThumb, getContext());
+
+
+                        // click adapter to chat
+                        holder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent chatIntent = new Intent(getContext(), RealtimeDbChatActivity.class);
+                                chatIntent.putExtra("user_id", list_user_id);
+                                chatIntent.putExtra("user_name", userName);
+                                startActivity(chatIntent);
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                // click avatar to show profile
+                holder.userImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(getActivity(), Profile.class);
+                        i.putExtra("uid", list_user_id);
+                        startActivity(i);
+                    }
+                });
+            }
+        };
+        mConvList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
